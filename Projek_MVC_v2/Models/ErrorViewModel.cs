cef0bfa94c5b6e5476f98f06b2389c5fa5113b75
Patryk Jancy <paxior788@gmail.com>
2020-04-04 18:28:12 +0200

    Add project files.

---
+++ Projek_MVC_v2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projek_MVC_v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
