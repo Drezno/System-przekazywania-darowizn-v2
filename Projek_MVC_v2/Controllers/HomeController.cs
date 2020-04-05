cc22e26d95dd9ff8f55c6126462ba33c7da1ce27
Patryk Jancy <paxior788@gmail.com>
2020-04-05 20:35:02 +0200

    przykładowy layout strony głównej - narazie przekierowuje do tego samego obrazu

--- Projek_MVC_v2/Controllers/HomeController.cs
+++ Projek_MVC_v2/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Projek_MVC_v2.Controllers
             return View();
         }
 
+        public IActionResult SupportCampaign()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
