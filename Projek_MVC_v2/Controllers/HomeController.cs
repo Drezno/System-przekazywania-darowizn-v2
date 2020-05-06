3d11fee5d0455383beebd38b0e89515b14d343ee
Patryk Jancy <paxior788@gmail.com>
2020-05-06 20:32:33 +0200

    nie działa przekazanie parametrow -,-

--- Projek_MVC_v2/Controllers/HomeController.cs
+++ Projek_MVC_v2/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using System.Web.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Projek_MVC_v2.Models;
@@ -36,6 +39,48 @@ namespace Projek_MVC_v2.Controllers
         [HttpGet]
         public IActionResult DonorForm()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Donation(Projek_MVC_v2.Models.DonorFormModel model)
+        {
+            string mail = model.Get("");
+
+            //string mm = Request["postal_code"];
+            /*string imie = Request["firstName"];
+            string subject = "Przekazanie darowizny";
+
+            MailMessage mm = new MailMessage();
+            mm.To.Add("paxior788@gmail.com");
+            mm.Subject = subject;
+            mm.Body = imie;
+            mm.From = new MailAddress("dobrowraca1997@gmail.com");
+            mm.IsBodyHtml = false;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = true;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new System.Net.NetworkCredential("dobrowraca1997@gmail.com", "poziom1C");
+            smtp.Send(mm);
+
+
+            /*MailMessage mail = new MailMessage("dobrowraca1997@gmail.com", "paxior788@gmail.com");
+            mail.Subject = "Przekazano darowiznę";
+            mail.Body = "DANE DARCZYŃCY: "+model.Email;
+            mail.IsBodyHtml = false;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+
+            NetworkCredential nc = new NetworkCredential("dobrowraca1997@gmail.com", "poziom1C");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = nc;
+            smtp.Send(mail);*/
+
+            
+
             return View();
         }
 
--- Projek_MVC_v2/Models/DonorFormModel.cs
+++ Projek_MVC_v2/Models/DonorFormModel.cs
@@ -1,17 +1,35 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Encodings.Web;
+using MimeKit;
+using MailKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using System.IO;
+using MimeKit.Utils;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Net.Mail;
 
 namespace Projek_MVC_v2.Models
 {
     public class DonorFormModel
     {
+        //public DonorFormModel()
+        //{
+
+        //}
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required]
         [DataType(DataType.Text)]
         public string First_Name { get; set; }
 
