48e5b74272369a7206f757f86e8c1b70688cd496
ShadowBean <damianponiedzialek00@gmail.com>
2020-05-20 21:41:22 +0200

    Organizacha moze usówać swje kampaniee

--- Projek_MVC_v2/Areas/Identity/Pages/Account/Manage/MyCampania.cshtml.cs
+++ Projek_MVC_v2/Areas/Identity/Pages/Account/Manage/MyCampania.cshtml.cs
@@ -1,16 +1,46 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using Projek_MVC_v2.Models;
 
 namespace Projek_MVC_v2.Areas.Identity.Pages.Account.Manage
 {
     public class MyCampaniaModel : PageModel
     {
-        public void OnGet()
+
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
         {
+
+
+
+            [Display(Name = "IdKampanii")]
+            public string idKampania { get; set; }
+
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            DatabaseConnection db = new DatabaseConnection();
+            MySqlConnection cnn = new MySqlConnection(db.GetConString());
+            var command = cnn.CreateCommand();
+            command.CommandText = "DELETE FROM kampanie where idkampanie='" + Input.idKampania + "'";
+            cnn.Open();
+            command.ExecuteNonQuery();
+            cnn.Close();
+
+            
+           
+            return RedirectToPage();
         }
     }
 }
