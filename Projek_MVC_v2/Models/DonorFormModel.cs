f10746d5284f0ed0e7dbcbb98d852a98b8c9759c
ShadowBean <damianponiedzialek00@gmail.com>
2020-05-27 21:59:38 +0200

    daj juz spokoj z ta data

--- Projek_MVC_v2/Models/DonorFormModel.cs
+++ Projek_MVC_v2/Models/DonorFormModel.cs
@@ -65,6 +65,7 @@ namespace Projek_MVC_v2.Models
         public string PostalCode { get; set; }
 
         [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
